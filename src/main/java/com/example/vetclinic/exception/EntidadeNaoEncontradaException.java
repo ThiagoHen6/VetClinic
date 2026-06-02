217074e01ad2c910cbdfbea4dd012bd75303ed5b
Quing_S2 <megat20150906@gmail.com>
2026-06-02 17:25:52 -0300

    GlobalAdvice feito

---
+++ src/main/java/com/example/vetclinic/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.example.vetclinic.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
